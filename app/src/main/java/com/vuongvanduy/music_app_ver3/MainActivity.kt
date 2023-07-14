0685736320a2ed84494eb1f5d2b0aca06012f48b
Vuong Van Duy <vuongvanduyit03@gmail.com>
2023-07-14 09:32:46 +0700

    add library

---
+++ app/src/main/java/com/vuongvanduy/music_app_ver3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vuongvanduy.music_app_ver3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
